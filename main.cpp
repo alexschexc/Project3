b94b6936f60160f52b98d393b06cbba672517288
Alex Schexnayder <alex.schex@gmail.com>
2024-07-30 14:15:39 -0500

    refactored code for readability

--- main.cpp
+++ main.cpp
@@ -45,6 +45,85 @@ sem_t emptySlots;
 sem_t fullSlots;
 
 /* Function used to add an item to the buffer */
+
+void add( int number );
+int remove( void ); 
+void producer( void );
+void consumer( void );
+
+/* The main function */
+int main( void )
+{
+ 
+   /* Initialize the buffer */
+   for(int i = 0; i < CAPACITY; ++i )
+   {
+      buffer[i] = 0;
+   }
+
+   /* Initialize the variables associated with the buffer */
+   head = 0;
+   tail = 0;
+   count = 0;
+
+   /* Initialize the semaphores */
+   sem_init( &pcMutex, 0, 1 );
+   sem_init( &emptySlots, 0, CAPACITY );
+   sem_init( &fullSlots, 0, 0 );
+
+   /* Create producer and consumer threads */
+   for( int i = 0; i < PRODUCERS; ++i )
+   {
+      producers[i] = thread ( producer );
+   }
+   for( int i = 0; i < CONSUMERS; ++i )
+   {
+      consumers[i] = thread ( consumer );
+   }
+
+   /* Wait for all threads to terminate */
+   for( int i = 0; i < PRODUCERS; ++i )
+   {
+      producers[i].join( );
+   }
+   for( int i = 0; i < CONSUMERS; ++i )
+   {
+      consumers[i].join( );
+   }
+   //////////////
+   /* Stage 1 */
+   /////////////
+
+
+
+   //////////////
+   /* Stage 2 */
+   /////////////  
+
+   
+   //////////////
+   /* Stage 3 */
+   /////////////
+
+
+   //////////////
+   /* Stage 4 */
+   /////////////
+
+
+   //////////////
+   /* Stage 5 */
+   /////////////
+
+
+
+   /* Destroy the semaphores */
+   sem_destroy( &pcMutex );
+   sem_destroy( &emptySlots );
+   sem_destroy( &fullSlots );
+
+   return 0;
+}
 void add( int number ) 
 {
 
@@ -122,55 +201,4 @@ void consumer( void )
       int number = remove( );
 	  
    }
-}
-
-
-/* The main function */
-int main( void )
-{
- 
-   /* Initialize the buffer */
-   for(int i = 0; i < CAPACITY; ++i )
-   {
-      buffer[i] = 0;
-   }
-
-   /* Initialize the variables associated with the buffer */
-   head = 0;
-   tail = 0;
-   count = 0;
-
-   /* Initialize the semaphores */
-   sem_init( &pcMutex, 0, 1 );
-   sem_init( &emptySlots, 0, CAPACITY );
-   sem_init( &fullSlots, 0, 0 );
-
-   /* Create producer and consumer threads */
-   for( int i = 0; i < PRODUCERS; ++i )
-   {
-      producers[i] = thread ( producer );
-   }
-   for( int i = 0; i < CONSUMERS; ++i )
-   {
-      consumers[i] = thread ( consumer );
-   }
-
-   /* Wait for all threads to terminate */
-   for( int i = 0; i < PRODUCERS; ++i )
-   {
-      producers[i].join( );
-   }
-   for( int i = 0; i < CONSUMERS; ++i )
-   {
-      consumers[i].join( );
-   }
-
-
-
-   /* Destroy the semaphores */
-   sem_destroy( &pcMutex );
-   sem_destroy( &emptySlots );
-   sem_destroy( &fullSlots );
-
-   return 0;
 }
